4143f677486f9432b59fb2ff4ab2294e2164022f
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-08-12 14:35:44 -0300

    feat: api created

---
+++ api/src/api.js
@@ -0,0 +1,27 @@
+var router = require("./router");
+
+var app = router(8080);
+
+var contatos = [
+  {nome: "Pedro", telefone: "99998888", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
+  {nome: "Ana", telefone: "99998877", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
+  {nome: "Maria", telefone: "99998866", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
+];
+
+var operadoras = [
+    {nome: "Oi", codigo: 14, categoria: "Celular", preco: 100},
+    {nome: "Vivo", codigo: 15, categoria: "Celular", preco: 120},
+    {nome: "Tim", codigo: 41, categoria: "Celular", preco: 110},
+    {nome: "GVT", codigo: 25, categoria: "Fixo", preco: 60},
+    {nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 40}
+  ];
+
+  app.get("/contatos", function(req, res) {
+    res.write(JSON.stringify(contatos));
+    res.end();
+  });
+
+  app.get("/operadoras", function(req, res) {
+    res.write(JSON.stringify(operadoras));
+    res.end();
+  });
