dc24ad747190b28b7d5184331f9fdfcc56f824b7
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-08-19 17:59:00 -0300

    fix: interceptors fixed

--- front/js/interceptors/timestampInterceptor.js
+++ front/js/interceptors/timestampInterceptor.js
@@ -1,11 +1,10 @@
 angular.module("listaTelefonica").factory("timestampInterceptor", function () {
 	return {
 		request: function (config) {
-			// var url = config.url;
-			// if (url.indexOf('view') > -1) return config;
-			// var timestamp = new Date().getTime();
-			// config.url = url + "?timestamp=" + timestamp;
-			console.log(config)
+			var url = config.url;
+			if (url.indexOf('view') > -1) return config;
+			var timestamp = new Date().getTime();
+			config.url = url + "?timestamp=" + timestamp;
 			return config;
 		}
 	};
