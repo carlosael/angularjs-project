8e9dbdde515cf34806c1586f04a749cd0364e593
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-08-19 13:53:06 -0300

    feat: ngRoute added

---
+++ front/js/controllers/novoContatoCtrl.js
@@ -0,0 +1,22 @@
+angular.module("listaTelefonica").controller("novoContatoCtrl", function ($scope, contatosAPI, operadorasAPI, serialGenerator, $location) {
+  $scope.app = "Lista Telefonica";
+  $scope.contatos = [];
+  $scope.operadoras = []; 
+
+  var carregarOperadoras = function () {
+    operadorasAPI.getOperadoras().then(function (response) {
+      $scope.operadoras = response.data;
+    });
+  };  
+  
+  $scope.adicionarContato = function (contato) {
+    contato.serial = serialGenerator.generate();
+    contatosAPI.saveContato(contato).then(function (response) {
+      delete $scope.contato;
+      $scope.contatoForm.$setPristine();
+      $location.path("/contatos");
+    });
+  };
+ 
+  carregarOperadoras();
+})
